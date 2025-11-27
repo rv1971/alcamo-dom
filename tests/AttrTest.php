81e8a2e864a5b04a65d1570ed0b2e3cc60de128b
rv1971 <rv1971@web.de>
2025-11-27 13:48:43 +0100

    WIP create new tests in namespace alcamo\dom

---
+++ tests/AttrTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace alcamo\dom;
+
+use alcamo\uri\FileUriFactory;
+use PHPUnit\Framework\TestCase;
+
+class AttrTest extends TestCase
+{
+    public const DATA_DIR = __DIR__ . DIRECTORY_SEPARATOR;
+
+    /**
+     * @dataProvider getDataProvider
+     */
+    public function testGetData(
+        $attr,
+        $expectedText,
+        $expectedBaseUri,
+        $expectedResolvedUri,
+        $expectedXName,
+        $expectedRfc5147Fragment,
+        $expectedRfc5147Uri
+    ): void {
+        /* This also tests the traits HavingBaseUriTrait, HavingXNameTrait,
+         * Rfc5147Trait. */
+
+        $this->assertSame((string)$expectedText, (string)$attr);
+
+        $this->assertSame((string)$expectedText, $attr->getValue());
+
+        $this->assertSame(
+            (string)$expectedBaseUri,
+            (string)$attr->getBaseUri()
+        );
+
+        $this->assertSame(
+            (string)$expectedResolvedUri,
+            (string)$attr->resolveUri('README.md')
+        );
+
+        $this->assertSame(
+            $expectedXName,
+            (string)$attr->getXName()
+        );
+
+        $this->assertSame(
+            $expectedRfc5147Fragment,
+            $attr->getRfc5147Fragment()
+        );
+
+        $this->assertSame(
+            $expectedRfc5147Uri,
+            $attr->getRfc5147Uri()
+        );
+    }
+
+    public function getDataProvider(): array
+    {
+        $fileUriFactory = new FileUriFactory(null, false);
+
+        $fooDoc = Document::newFromUri(
+            $fileUriFactory->create(self::DATA_DIR . 'foo.xml')
+        );
+
+        return [
+            [
+                $fooDoc['bar']->getAttributeNodeNS(Document::XML_NS, 'base'),
+                'http://bar.example.biz',
+                'http://bar.example.biz',
+                'http://bar.example.biz/README.md',
+                Document::XML_NS . ' base',
+                'line=7',
+                $fooDoc->documentURI . '#line=7'
+            ],
+            [
+                $fooDoc['bar']->firstChild->getAttributeNode('id'),
+                'A',
+                'http://bar.example.biz',
+                'http://bar.example.biz/README.md',
+                'id',
+                'line=8',
+                $fooDoc->documentURI . '#line=8'
+            ]
+        ];
+    }
+}
