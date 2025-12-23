43377d68b60b21886f9c858f4a167e535429dbba
rv1971 <rv1971@web.de>
2025-12-23 12:03:59 +0100

    Fix XPath::__construct()

--- src/XPath.php
+++ src/XPath.php
@@ -7,14 +7,14 @@ namespace alcamo\dom;
  *
  * @date Last reviewed 2025-09-15
  */
-class XPath extends \DOMXPath implements NamespaceConstantsInterface
+class XPath extends \DOMXPath
 {
     /// Register all namepace prefixes in NamespaceConstantsInterface
     public function __construct(\DOMDocument $doc)
     {
         parent::__construct($doc);
 
-        foreach (static::NS_PRFIX_TO_NS_URI as $prefix => $uri) {
+        foreach ($doc::NS_PRFIX_TO_NS_URI as $prefix => $uri) {
             $this->registerNamespace($prefix, $uri);
         }
     }
