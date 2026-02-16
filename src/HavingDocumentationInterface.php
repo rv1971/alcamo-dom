e6a39ccc4011c519ad736c4458d7521bf7fa4775
rv1971 <rv1971@web.de>
2026-02-16 19:17:19 +0100

    Code formatting

--- src/HavingDocumentationInterface.php
+++ src/HavingDocumentationInterface.php
@@ -10,16 +10,16 @@ namespace alcamo\dom;
 interface HavingDocumentationInterface
 {
     /// Fallback to another language if the requested one is not available
-    public const FALLBACK_TO_OTHER_LANG = 1;
+    public const FALLBACK_TO_OTHER_LANG       = 1;
 
     /// Fallback to the fragment part of owl:sameAs, if any
-    public const FALLBACK_TO_SAME_AS_FRAGMENT  = 2;
+    public const FALLBACK_TO_SAME_AS_FRAGMENT = 2;
 
     /// Fallback to the name of the node's type, if any
-    public const FALLBACK_TO_TYPE_NAME  = 4;
+    public const FALLBACK_TO_TYPE_NAME        = 4;
 
     /// Fallback to the name of the node, if any
-    public const FALLBACK_TO_NAME       = 8;
+    public const FALLBACK_TO_NAME             = 8;
 
     /**
      * @brief Get a human-readable label, if available
